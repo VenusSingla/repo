d09e9b59782b10fbde3c9c9cc5c4fa3a668d3640
VenusSingla <86290496+VenusSingla@users.noreply.github.com>
2025-05-24 17:46:40 +0530

    Update app_code_v1.py

--- app_code_v1.py
+++ app_code_v1.py
@@ -18,7 +18,10 @@ scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-credentials = Credentials.from_service_account_file("Credentials1.json", scopes=scope)
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scopes
+)
 client = gspread.authorize(credentials)
 worksheet = client.open("ISL Feedback").sheet1  # You can also use .worksheet("Sheet1")
 # worksheet = "https://script.google.com/a/macros/thapar.edu/s/AKfycbzBnlbsz0zMr_563kR8b50nnP7_vLieLsupqNsBWCUdo6dQ5JBpwwGxiXTlPj1fB8ezIw/exec"
